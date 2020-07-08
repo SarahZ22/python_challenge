d41279b9f42f03dbe7d3c8c7d0a64dc1d90eaf58
SarahZ22 <65513913+SarahZ22@users.noreply.github.com>
2020-07-07 22:16:05 -0600

    PyBank Update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,82 +1,70 @@
 import os
 import csv
 
+#lists/variables to hold info
+months_tot = []
+profits_losses = []
+change_pl = []
+avg_change = 0
+previous_month = 0
+greatestinc = 0
+greatestdec = 0
+
 #path to folder
-election_data = os.path.join('Resources', 'election_data.csv')
+budget_data = os.path.join('Resources', 'budget_data.csv')
 
 #open and read the file
-with open(election_data, 'r') as csvfile:
+with open(budget_data, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     #skip header
     csvhead = next(csvreader)
+    
+   #loop through all data & put info into the lists
+    for row in csvreader:
+        months_tot.append(row[0])
+        profits_losses.append(int(row[1]))
+  
+        #calculate changes from month to month 
+        avg_change +=1
 
-    #lists & variables to store data
-    candidates = {}
-    votes = []
-    votespercand = 0
-    total_votes = 0
-    percent_votes = 0
-    most_votes = 0
+        if (avg_change > 1):
+            change_pl.append(float(row[1]) - previous_month)
+        previous_month = float(row[1])
 
-    #loop through data to get all candidates & votes
-    for row in csvreader: 
+    #calculate total months
+    allmonths = len(months_tot)
+    
+    totalPL = sum(profits_losses)
+    totalPL = '${}'.format(totalPL)
 
-        candidate = row[2]
-        #add votes to list and add to vote count
-        votes.append(total_votes)
-        total_votes +=1
+    #calculate average change
+    average_chg = (sum(change_pl) / len(change_pl))
+    average_chg = '${:,.2f}'.format(average_chg)
 
-        #separate out candidates - add one each time the candidate appears
-        if candidate in candidates:
-            candidates[candidate] +=1
-        else:
-            candidates[candidate] = 1
+    #find greatest profit
+    greatest_inc = max(change_pl)
+    inc_month = months_tot[change_pl.index(greatest_inc) + 1 ]
+    greatest_inc = '${}'.format(greatest_inc)
+    
+    #find greatest decrease
+    greatest_dec = min(change_pl)
+    dec_month = months_tot[change_pl.index(greatest_dec) +1 ]
+    greatest_dec = '${}'.format(greatest_dec)
+    
 
-#count number of votes for each candidate
-for candidate in candidates:       
-
-    #to determine winner, if has most votes...
-    if candidates[candidate] > most_votes:
-        most_votes = candidates[candidate]
-        winner = candidate         
-
-#print results to terminal
-print('Election Results')
-print('-'*30)
-print('Total Votes: ' + str(total_votes))
-print('-'*30)
-
-#calculate % of votes for each candidate - only way I can get it to print correctly
-for candidate in candidates:
-    percent_votes = (candidates[candidate])/total_votes *100
-    percent_votes = "{}% ".format(round(percent_votes, 3))
-    print(f'{candidate}: {percent_votes} ({candidates[candidate]})') 
-
-print('-'*30)
-print('Winner: ' + str(winner))
-print('-'*30)
+    #print results to terminal
+    print('Financial Analysis')
+    print('-'*30)
+    print(f'Total Months:', allmonths)
+    print(f'Total:', totalPL)
+    print(f'Average Change:', average_chg)
+    print(f'Greatest Increase in Profits:', (inc_month + " " + "(" + greatest_inc + ")"))
+    print(f'Greatest Decrease in Profits:', (dec_month + " " + "(" + greatest_dec + ")"))
+    
+#combine print statements to print in txt file
+output = ('Financial Analysis' + '\n' +'-'*30 + '\n' + 'Total Months: ' + str(allmonths) + '\n' + 'Total: ' + totalPL + '\n' 'Average Change: ' + average_chg + '\n' + 'Greatest Increase in Profits:' + (inc_month + " " + "(" + greatest_inc + ")") + '\n' + 'Greatest Decrease in Profits:' + (dec_month + " " + "(" + greatest_dec + ")"))
 
 #write to txt file
-#set location for output
-outputplace = os.path.join('Analysis', 'Election_Results.txt')
-
-    # #open file to write
-with open(outputplace, 'w', newline ='') as datafile:
-    txtwriter = csv.writer(datafile, delimiter= ' ')
-
-    #write to the file
-    txtwriter.writerow(['Election Results'])
-    txtwriter.writerow(['-'*30])
-    txtwriter.writerow(['Total Votes:', total_votes])
-    txtwriter.writerow(['-'*30])
-
-    #to print % correctly
-    for candidate in candidates:
-        percent_votes = (candidates[candidate])/total_votes *100
-        percent_votes = "{}% ".format(round(percent_votes, 3))
-        txtwriter.writerow([f'{candidate}: {percent_votes} ({candidates[candidate]})'])
-        
-    txtwriter.writerow(['-'*30])
-    txtwriter.writerow(['Winner: ', winner])
-    txtwriter.writerow(['-'*30])
+with open('Analysis/Financial_analysis.txt', 'w') as tf:
+    tf.write(output)
